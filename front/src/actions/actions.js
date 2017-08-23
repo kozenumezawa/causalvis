20ef3280171949dd8ff092d60820f707722377a2
kozenumezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-08-23 17:00:18 +0900

    Update: change ESLint rules

--- front/src/actions/actions.js
+++ front/src/actions/actions.js
@@ -1,10 +1,11 @@
-export const ADD_TEXT = 'ADD_TEXT'
+export const ADD_TEXT = 'ADD_TEXT';
 let textId = 1;
 
 export function addText(newText) {
+  textId += 1;
   return {
     type: ADD_TEXT,
-    id: textId++,
-    text: newText
+    id: textId,
+    text: newText,
   };
 }
