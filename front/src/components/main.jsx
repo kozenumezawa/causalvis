8e20469ac08a1cd3683f17cf285a9129e8437f59
kozenumezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-08-23 13:24:09 +0900

    Add: introduce semantic-UI and its sample

--- front/src/components/main.jsx
+++ front/src/components/main.jsx
@@ -4,14 +4,19 @@ import { connect } from 'react-redux';
 
 import { addText } from '../actions/actions';
 
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react';
+
 class Main extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //
-  //   this.onAddBtnClicked = this.onAddBtnClicked.bind(this);
-  // }
+  constructor(props) {
+    super(props);
+
+    this.onAddBtnClicked = this.onAddBtnClicked.bind(this);
+    this.state = { visible: true };
+  }
 
   render() {
+    const { activeItem } = this.state || {}
+
     return (
       <div>
         <Switch>
@@ -19,22 +24,72 @@ class Main extends React.Component {
             exact path="/"
             render={() => (
               <div>
-                <input type="text" ref="input" />
-                <br />
-                <button onClick={(e) => this.onAddBtnClicked(e)}>
-                  Add
-                </button>
-                <ul>
-                  {
-                    this.props.state.storedText.map((obj) => {
-                      return (
-                        <li key={obj.id}>
-                          { obj.text }
-                        </li>
-                      );
-                    })
-                  }
-                </ul>
+                <Sidebar.Pushable as={Segment}>
+                  <Sidebar as={Menu} animation='uncover' width='thin' visible={this.state.visible} icon='labeled' vertical inverted>
+                    <Menu.Item>
+                      <Menu.Header>Products</Menu.Header>
+
+                      <Menu.Menu>
+                        <Menu.Item name='enterprise' active={activeItem === 'enterprise'} onClick={this.handleItemClick} />
+                        <Menu.Item name='consumer' active={activeItem === 'consumer'} onClick={this.handleItemClick} />
+                      </Menu.Menu>
+                    </Menu.Item>
+
+                    <Menu.Item>
+                      <Menu.Header>CMS Solutions</Menu.Header>
+
+                      <Menu.Menu>
+                        <Menu.Item name='rails' active={activeItem === 'rails'} onClick={this.handleItemClick} />
+                        <Menu.Item name='python' active={activeItem === 'python'} onClick={this.handleItemClick} />
+                        <Menu.Item name='php' active={activeItem === 'php'} onClick={this.handleItemClick} />
+                      </Menu.Menu>
+                    </Menu.Item>
+
+                    <Menu.Item>
+                      <Menu.Header>Hosting</Menu.Header>
+
+                      <Menu.Menu>
+                        <Menu.Item name='shared' active={activeItem === 'shared'} onClick={this.handleItemClick} />
+                        <Menu.Item name='dedicated' active={activeItem === 'dedicated'} onClick={this.handleItemClick} />
+                      </Menu.Menu>
+                    </Menu.Item>
+
+                    <Menu.Item>
+                      <Menu.Header>Support</Menu.Header>
+
+                      <Menu.Menu>
+                        <Menu.Item name='email' active={activeItem === 'email'} onClick={this.handleItemClick}>
+                          E-mail Support
+                        </Menu.Item>
+
+                        <Menu.Item name='faq' active={activeItem === 'faq'} onClick={this.handleItemClick}>
+                          FAQs
+                        </Menu.Item>
+                      </Menu.Menu>
+                    </Menu.Item>
+                  </Sidebar>
+                  <Sidebar.Pusher>
+                    <Segment basic style={{ height: 500 }}>
+                      <input type="text" ref="input" />
+                      <br />
+                      <button onClick={(e) => this.onAddBtnClicked(e)}>
+                        Add
+                      </button>
+                      <ul>
+                        {
+                          this.props.state.storedText.map((obj) => {
+                            return (
+                              <li key={obj.id}>
+                                { obj.text }
+                              </li>
+                            );
+                          })
+                        }
+                      </ul>
+                      <Button>Hello</Button>
+                    </Segment>
+                  </Sidebar.Pusher>
+                </Sidebar.Pushable>
               </div>
             )}
           />
---
+++ front/src/components/sidebar/side-bar.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+
+class SidebarLeftUncover extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { visible: true }
+  }
+
+  render() {
+    const { visible } = this.state
+    return (
+      <div>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='uncover' width='thin' visible={visible} icon='labeled' vertical inverted>
+            <Menu.Item name='home'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item name='gamepad'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item name='camera'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>Application Content</Header>
+              <Image src='/assets/images/wireframe/paragraph.png' />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    )
+  }
+}
+
+export default SidebarLeftUncover
