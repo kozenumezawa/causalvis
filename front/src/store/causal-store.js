398c0577791b4732879e23dfc101c6b3f205ebfe
kozenumezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-08-30 20:05:14 +0900

    Update: to fetch causal matrix

--- front/src/store/causal-store.js
+++ front/src/store/causal-store.js
@@ -2,41 +2,41 @@ import Rx from 'rx';
 
 import { FETCH_TIFF } from '../constants/event-constants';
 
-const fetchCausal = (allTimeSeries, width) => {
-  window.fetch('http://localhost:3000/api/v1/causal', {
-    mode: 'cors',
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      allTimeSeries: allTimeSeries,
-      width: width,
-      method: 'CROSS',
-    }),
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      console.log('json', json);
-    });
-};
-
-const store = (intentSubject, filterSubject) => {
+const store = (intentSubject, filterSubject, dataSubject) => {
   const state = {
     causalMatrix: [],
   };
 
   const subject = new Rx.BehaviorSubject({ state });
 
-  Rx.Observable.zip(intentSubject, filterSubject).subscribe(([payload, filter]) => {
+  Rx.Observable.zip(intentSubject, filterSubject, dataSubject).subscribe(([payload, filter, data]) => {
     if (filter.state.allTimeSeries.length === 0) {
       subject.onNext({ state });
       return;
     }
-    state.causalMatrix = fetchCausal(filter.state.allTimeSeries, filter.state.width);
-    subject.onNext({ state });
+
+    window.fetch('http://localhost:3000/api/v1/causal', {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        allTimeSeries: filter.state.allTimeSeries,
+        width: data.state.width,
+        maxLag: 20,
+        lagStep: 2,
+        meanStep: 3,
+        method: 'CROSS',
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        state.causalMatrix = json.causalMatrix;
+        subject.onNext({ state });
+      });
   });
   return subject;
 };
--- front/src/store/filter-store.js
+++ front/src/store/filter-store.js
@@ -5,14 +5,12 @@ import { FETCH_TIFF } from '../constants/event-constants';
 const store = (intentSubject, dataSubject) => {
   const state = {
     allTimeSeries: [],
-    width: 0,
   };
 
   const subject = new Rx.BehaviorSubject({ state });
 
   Rx.Observable.zip(intentSubject, dataSubject).subscribe(([payload, data]) => {
     state.allTimeSeries = data.state.allTimeSeries;
-    state.width = data.state.width;
 
     switch (payload.type) {
       case FETCH_TIFF:
