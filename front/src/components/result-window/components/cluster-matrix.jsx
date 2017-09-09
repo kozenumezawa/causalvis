347f385dc4c6b061ffe0176acdcb22955f3d1fbd
kozenumezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-09-09 19:19:49 +0900

    Add: add a new overlay canvas to cluster view

--- front/src/components/result-window/components/cluster-matrix.jsx
+++ front/src/components/result-window/components/cluster-matrix.jsx
@@ -14,12 +14,15 @@ export default class ClusterMatrix extends React.Component {
   componentDidMount() {
     this.clusterCanvas = document.getElementById(`cluster_canvas_${this.props.id}`);
     this.clusterCtx = this.clusterCanvas.getContext('2d');
+    this.clusterOverlayCanvas = document.getElementById(`cluster_canvas_${this.props.id}_overlay`);
+    this.clusterOverlayCtx = this.clusterOverlayCanvas.getContext('2d');
 
     this.heatmapCanvas = document.getElementById(`heatmap_canvas_${this.props.id}`);
     this.heatmapCtx = this.heatmapCanvas.getContext('2d');
-    this.heatmapCanvasOverlay = document.getElementById(`heatmap_canvas_${this.props.id}_overlay`);
+    this.heatmapOverlayCanvas = document.getElementById(`heatmap_canvas_${this.props.id}_overlay`);
+    this.heatmapOverlayCtx = this.heatmapOverlayCanvas.getContext('2d');
 
-    this.heatmapCanvas.addEventListener('mousemove', this.onMouseMoveHeatmap.bind(this));
+    this.heatmapOverlayCanvas.addEventListener('mousemove', this.onMouseMoveHeatmap.bind(this));
   }
 
   componentWillReceiveProps(nextProps) {
@@ -30,6 +33,29 @@ export default class ClusterMatrix extends React.Component {
     const rect = e.target.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
+
+    this.heatmapOverlayCtx.strokeStyle = 'black';
+
+    this.heatmapOverlayCtx.clearRect(0, 0, this.heatmapOverlayCanvas.width, this.heatmapOverlayCanvas.height);
+    this.heatmapOverlayCtx.beginPath();
+
+    this.heatmapOverlayCtx.moveTo(this.legendWidth, y);
+    this.heatmapOverlayCtx.lineTo(this.heatmapOverlayCanvas.width, y);
+
+    this.heatmapOverlayCtx.moveTo(x, this.legendWidth);
+    this.heatmapOverlayCtx.lineTo(x, this.heatmapOverlayCanvas.height);
+
+    this.heatmapOverlayCtx.closePath();
+    this.heatmapOverlayCtx.stroke();
+
+    const causeIdx = y - this.legendWidth;
+    const effectIdx = x - this.legendWidth;
+
+    if (causeIdx < 0 || effectIdx < 0) {
+      return;
+    }
+
+    console.log(this.clusterSampledCoords[causeIdx]);
   }
 
   drawData(props) {
@@ -40,28 +66,33 @@ export default class ClusterMatrix extends React.Component {
     const meanStep = props.meanR * 2 + 1;
 
     // draw heatmap and canvas according to the graph
-    const graphSorted = props.clusterMatrix;
-    const nClusterList = props.nClusterList;
-    const clusterSampledCoords = props.clusterSampledCoords;
-    const color = drawingTool.getColorCategory(nClusterList.length);
+    this.graphSorted = props.clusterMatrix;
+    this.nClusterList = props.nClusterList;
+    this.clusterSampledCoords = props.clusterSampledCoords;
+    const color = drawingTool.getColorCategory(this.nClusterList.length);
 
     // draw a canvas
     this.clusterCanvas.width = props.width * props.scale;
     this.clusterCanvas.height = props.allTimeSeries.length / props.width * props.scale;
     drawingTool.drawFrame(this.clusterCanvas, this.clusterCtx);
+    this.clusterOverlayCanvas.width = this.clusterCanvas.width;
+    this.clusterOverlayCanvas.height = this.clusterCanvas.height;
     // this.clusterCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, this.clusterCanvas.width, this.clusterCanvas.height);
 
 
     const cellSize = 0.5 * props.scale / 2;
 
-    this.heatmapCanvas.width = graphSorted.length * cellSize + this.legendWidth;
-    this.heatmapCanvas.height = graphSorted.length * cellSize + this.legendWidth;
-    this.heatmapCanvasOverlay.width = this.heatmapCanvas.width;
-    this.heatmapCanvasOverlay.height = this.heatmapCanvas.height;
+    this.heatmapCanvas.width = this.graphSorted.length * cellSize + this.legendWidth;
+    this.heatmapCanvas.height = this.graphSorted.length * cellSize + this.legendWidth;
+    this.heatmapOverlayCanvas.width = this.heatmapCanvas.width;
+    this.heatmapOverlayCanvas.height = this.heatmapCanvas.height;
+
+    this.heatmapCanvas.style.left = `${this.clusterCanvas.width}px`;
+    this.heatmapOverlayCanvas.style.left = `${this.clusterCanvas.width}px`;
 
     // save start and stop index of each cluster
     const clusterRangeList = [];
-    nClusterList.reduce((prev, current) => {
+    this.nClusterList.reduce((prev, current) => {
       const endPixel = prev + current;
       clusterRangeList.push({
         start: prev,
@@ -72,7 +103,7 @@ export default class ClusterMatrix extends React.Component {
 
     // draw heatmap and fill canvas
     let clusterIdx = 0;
-    graphSorted.forEach((row, rowIdx) => {
+    this.graphSorted.forEach((row, rowIdx) => {
       if (rowIdx >= clusterRangeList[clusterIdx].end) {
         clusterIdx++;
       }
@@ -88,18 +119,18 @@ export default class ClusterMatrix extends React.Component {
       // draw the canvas according to the cluster
       this.clusterCtx.fillStyle = color[clusterIdx];
 
-      const x = clusterSampledCoords[rowIdx].x * props.scale;
-      const y = clusterSampledCoords[rowIdx].y * props.scale;
+      const x = this.clusterSampledCoords[rowIdx].x * props.scale;
+      const y = this.clusterSampledCoords[rowIdx].y * props.scale;
       this.clusterCtx.fillRect(x - 1, y - 1, meanStep * props.scale, meanStep * props.scale);
     });
 
     // draw line and legend to the heat map
     drawingTool.drawFrame(this.heatmapCanvas, this.heatmapCtx);
-    this.heatmapCtx.line_color = 'black';
+    this.heatmapCtx.strokeStyle = 'black';
     this.heatmapCtx.lineWidth = 0.5;
     this.heatmapCtx.beginPath();
     let heatmapCtxX = this.legendWidth - 1;
-    nClusterList.forEach((nCluster, idx) => {
+    this.nClusterList.forEach((nCluster, idx) => {
       // draw legend
       this.heatmapCtx.fillStyle = color[idx];
       this.heatmapCtx.fillRect(heatmapCtxX, 0, nCluster * cellSize, this.legendWidth);
@@ -119,7 +150,7 @@ export default class ClusterMatrix extends React.Component {
 
     // 因果関係を表すcausal matrixを生成
     // const causalMatrix = [];
-    // nClusterList.forEach((nCluster, rowClusterIdx) => {
+    // this.nClusterList.forEach((nCluster, rowClusterIdx) => {
     //   causalMatrix.push([]);
     //   const rowRange = clusterRangeList[rowClusterIdx];
     //
@@ -132,13 +163,13 @@ export default class ClusterMatrix extends React.Component {
     //     let causalCnt = 0;
     //     for (let rowIdx = rowRange.start; rowIdx < rowRange.end; rowIdx++) {
     //       for (let colIdx = colRange.start; colIdx < colRange.end; colIdx++) {
-    //         if (graphSorted[rowIdx][colIdx] === true) {
+    //         if (this.graphSorted[rowIdx][colIdx] === true) {
     //           causalCnt++;
     //         }
     //       }
     //     }
     //
-    //     const area = nCluster * nClusterList[colClusterIdx];
+    //     const area = nCluster * this.nClusterList[colClusterIdx];
     //     if (causalCnt > area / 2.5) {
     //       causalMatrix[rowClusterIdx][colClusterIdx] = 1;
     //     } else {
@@ -156,18 +187,26 @@ export default class ClusterMatrix extends React.Component {
 
   render() {
     return (
-      <div style={{ position: 'relative', height: 300 }}>
+      <div style={{ height: 300 }} >
         <OriginalCanvas
           id={this.props.id}
           allTimeSeries={this.props.allTimeSeries}
           width={this.props.width}
           scale={this.props.scale}
         />
+
         <span style={{ marginRight: 20 }} />
-        <canvas id={`cluster_canvas_${this.props.id}`} />
+        <div style={{ position: 'relative', display: 'inline' }}>
+          <canvas id={`cluster_canvas_${this.props.id}`} style={{ position: 'absolute', zIndex: 1 }} />
+          <canvas id={`cluster_canvas_${this.props.id}_overlay`} style={{ position: 'absolute', zIndex: 2, left: 0 }} />
+        </div>
+
         <span style={{ marginRight: 20 }} />
-        <canvas id={`heatmap_canvas_${this.props.id}`} style={{ position: 'absolute', zIndex: 1 }} />
-        <canvas id={`heatmap_canvas_${this.props.id}_overlay`} style={{ position: 'absolute', zIndex: 2 }} />
+        <div style={{ position: 'relative', display: 'inline' }}>
+          <canvas id={`heatmap_canvas_${this.props.id}`} style={{ position: 'absolute', zIndex: 1 }} />
+          <canvas id={`heatmap_canvas_${this.props.id}_overlay`} style={{ position: 'absolute', zIndex: 2 }} />
+        </div>
+
       </div>
     );
   }
