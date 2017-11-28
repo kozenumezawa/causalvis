9de386fd33c047f92ce016db2034f875643ffdaf
kozenumezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-11-28 20:36:50 +0900

    Update: to conduct causal inference and clustering at the same time

--- front/src/store/clustering-store.js
+++ front/src/store/clustering-store.js
@@ -3,8 +3,9 @@ import Rx from 'rx';
 import { FETCH_TIFF, SET_NEWDATA } from '../constants/event-constants';
 import { DATA_SIM, DATA_TRP3 } from '../constants/general-constants';
 
-const store = (intentSubject, causalSubject, filterSubject) => {
+const store = (intentSubject, filterSubject) => {
   const state = {
+    causalMatrices: [],
     clusterMatrices: [],
     clusterSampledCoords: [],
     clusterRangeLists: [],
@@ -14,29 +15,33 @@ const store = (intentSubject, causalSubject, filterSubject) => {
 
   const subject = new Rx.BehaviorSubject({ state });
 
-  Rx.Observable.zip(intentSubject, causalSubject, filterSubject).subscribe(([payload, causal, filter]) => {
+  Rx.Observable.zip(intentSubject, filterSubject).subscribe(([payload, filter]) => {
     switch (payload.type) {
       case FETCH_TIFF: {
-        if (causal.state.causalMatrices[0] == null) {
+        if (filter.state.sampledAllTimeSeries[0] == null) {
           subject.onNext({ state });
           return;
         }
 
         const dataNames = [DATA_TRP3, DATA_SIM];
-        const fetchPromises = causal.state.causalMatrices.map((causalMatrix, idx) => {
-          const clusteringFetch = fetch(`${API_ENDPOINT}/api/v1/clustering`, {
+        const fetchPromises = filter.state.sampledAllTimeSeries.map((allTimeSeries, idx) => {
+          const clusteringFetch = fetch(`${API_ENDPOINT}/api/v1/causal-and-clustering`, {
             mode: 'cors',
             method: 'POST',
             headers: {
               'content-type': 'application/json',
             },
             body: JSON.stringify({
-              causalMatrix,
-              method: 'IRM',
+              allTimeSeries,
+              maxLag: 20,
+              lagStep: 2,
+              causalMethod: 'CROSS',
+              windowSize: filter.state.windowSize[idx],
+
+              clusteringMethod: 'IRM',
               threshold: 0.7,
               sampledCoords: filter.state.sampledCoords[idx],
               dataName: dataNames[idx],
-              windowSize: filter.state.windowSize[idx],
             }),
           })
             .then(response => response.json())
@@ -46,6 +51,7 @@ const store = (intentSubject, causalSubject, filterSubject) => {
 
         Promise.all(fetchPromises).then((responseJSONS) => {
           responseJSONS.forEach((json, idx) => {
+            state.causalMatrices[idx] = json.causalMatrix;
             state.clusterMatrices[idx] = json.clusterMatrix;
             state.clusterSampledCoords[idx] = json.clusterSampledCoords;
             state.nClusterLists[idx] = json.nClusterList;
@@ -63,6 +69,7 @@ const store = (intentSubject, causalSubject, filterSubject) => {
             }, 0);
             state.clusterRangeLists[idx] = clusterRangeList;
           });
+
           subject.onNext({ state });
         });
         break;
@@ -77,7 +84,7 @@ const store = (intentSubject, causalSubject, filterSubject) => {
             'content-type': 'application/json',
           },
           body: JSON.stringify({
-            causalMatrix: causal.state.causalMatrices[position],
+            // causalMatrix: causal.state.causalMatrices[position],
             method: 'IRM',
             threshold: 0.7,
             sampledCoords: filter.state.sampledCoords[position],
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,7 +1,6 @@
 import Rx from 'rx';
 import dataStore from './data-store';
 import filterStore from './filter-store';
-import causalStore from './causal-store';
 import clusteringStore from './clustering-store';
 import networkStore from './network-store';
 import canvasEventStore from './canvas-event-store';
@@ -14,8 +13,7 @@ const store = () => {
   const modalSubject = modalStore(intentSubject);
   const dataSubject = dataStore(intentSubject);
   const filterSubject = filterStore(intentSubject, dataSubject);
-  const causalSubject = causalStore(intentSubject, filterSubject);
-  const clusteringSubject = clusteringStore(intentSubject, causalSubject, filterSubject);
+  const clusteringSubject = clusteringStore(intentSubject, filterSubject);
   const networkSubject = networkStore(intentSubject, clusteringSubject);
   const canvasEventSubject =
     canvasEventStore(intentSubject, dataSubject, filterSubject, clusteringSubject);
@@ -23,16 +21,14 @@ const store = () => {
   return Rx.Observable.zip(
     dataSubject,
     filterSubject,
-    causalSubject,
     clusteringSubject,
     networkSubject,
     modalSubject,
     canvasEventSubject,
-    (data, filter, causal, clustering, network, modal, canvasEvent) => {
+    (data, filter, clustering, network, modal, canvasEvent) => {
       const states = {
         data: data.state,
         filter: filter.state,
-        causal: causal.state,
         clustering: clustering.state,
         network: network.state,
         modal: modal.state,
